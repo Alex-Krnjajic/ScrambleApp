d39602a3cc8f4fc9afe949e30930bf998c27e90e
Don-Cheadle <alexk753@gmail.com>
2020-05-14 13:32:53 +0200

    feature: you can now generate a bitmap with the camera in progress: scrambling the bitmap

--- app/src/main/java/com/example/scrambleapp/MainActivity.java
+++ app/src/main/java/com/example/scrambleapp/MainActivity.java
@@ -1,14 +1,83 @@
 package com.example.scrambleapp;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.app.Activity;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.Matrix;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity {
+    private static final int CAMERA_REQUEST = 1888;
+    ImageView imageView;
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        imageView = (ImageView) this.findViewById(R.id.imageView);
+        Button photoButton = (Button) this.findViewById(R.id.button);
+
+        photoButton.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                Intent cameraIntent = new Intent(android.provider.MediaStore.ACTION_IMAGE_CAPTURE);
+                startActivityForResult(cameraIntent, CAMERA_REQUEST);
+            }
+        });
+    }
+
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == CAMERA_REQUEST) {
+            Bitmap photo = (Bitmap) data.getExtras().get("data");
+            photo = getResizedBitmap(photo, 700, 700);
+            imageView.setImageBitmap(photo);
+
+            Bitmap[][] splitMap = splitBitmap(photo, 3, 3);
+            imageView.setImageBitmap(splitMap[0][0]);
+
+        }
+    }
+
+    public Bitmap getResizedBitmap(Bitmap bm, int newWidth, int newHeight) {
+        int width = bm.getWidth();
+        int height = bm.getHeight();
+        float scaleWidth = ((float) newWidth) / width;
+        float scaleHeight = ((float) newHeight) / height;
+        // CREATE A MATRIX FOR THE MANIPULATION
+        Matrix matrix = new Matrix();
+        // RESIZE THE BIT MAP
+        matrix.postScale(scaleWidth, scaleHeight);
+
+        // "RECREATE" THE NEW BITMAP
+        Bitmap resizedBitmap = Bitmap.createBitmap(
+                bm, 0, 0, width, height, matrix, false);
+        bm.recycle();
+        return resizedBitmap;
+    }
+
+    public Bitmap[][] splitBitmap(Bitmap bitmap, int xCount, int yCount) {
+        // Allocate a two dimensional array to hold the individual images.
+        Bitmap[][] bitmaps = new Bitmap[xCount][yCount];
+        int width, height;
+        // Divide the original bitmap width by the desired vertical column count
+        width = bitmap.getWidth() / xCount;
+        // Divide the original bitmap height by the desired horizontal row count
+        height = bitmap.getHeight() / yCount;
+        // Loop the array and create bitmaps for each coordinate
+        for (int x = 0; x < xCount; ++x) {
+            for (int y = 0; y < yCount; ++y) {
+                // Create the sliced bitmap
+                bitmaps[x][y] = Bitmap.createBitmap(bitmap, x * width, y * height, width, height);
+            }
+        }
+        // Return the array
+        return bitmaps;
+
     }
 }
